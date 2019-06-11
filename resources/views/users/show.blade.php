8d59c141c9a251629b41a6a27bc139a85cebc149
corbin <771438815@qq.com>
2019-06-11 15:45:28 +0000

    动态流

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,16 +4,20 @@
 @section('content')
 <div class="row">
   <div class="offset-md-2 col-md-8">
-    <div class="col-md-12">
-      <div class="offset-md-2 col-md-8">
-        <section class="user_info">
-          @include('shared._user_info', ['user' => $user])
-        </section>
-        <section class="status">
-            <section class="stats mt-2">
-		      @include('shared._stats', ['user' => $user])
-		    </section>
-		 @if ($statuses->count() > 0)
+    <section class="user_info">
+      @include('shared._user_info', ['user' => $user])
+    </section>
+
+    @if (Auth::check())
+      @include('users._follow_form')
+    @endif
+
+    <section class="stats mt-2">
+      @include('shared._stats', ['user' => $user])
+    </section>
+    <hr>
+    <section class="status">
+      @if ($statuses->count() > 0)
         <ul class="list-unstyled">
           @foreach ($statuses as $status)
             @include('statuses._status')
@@ -26,8 +30,6 @@
         <p>没有数据！</p>
       @endif
     </section>
-      </div>
-    </div>
   </div>
 </div>
 @stop
